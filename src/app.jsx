b314e81b47a152e57153227e37a92ed373fd34da
xypecho <xypecho@163.com>
2019-09-23 22:46:25 +0800

    react的基础知识好杂

--- src/app.jsx
+++ src/app.jsx
@@ -1,6 +1,13 @@
+/*
+ * @Author: xueyp
+ * @Date: 2019-09-23 21:39:05
+ * @Last Modified by: xueyp
+ * @Last Modified time: 2019-09-23 21:45:47
+ */
 import React from 'react';
 import ReactDom from 'react-dom';
 import './style.css';
+import './test.scss';
 
 ReactDom.render(
     <div>
