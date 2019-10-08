fe0dbfc7d271bb9e7434a9775125e12b5db626b5
xypecho <xypecho@163.com>
2019-10-08 22:54:14 +0800

    update

--- src/component/layout/component/navbar/index.jsx
+++ src/component/layout/component/navbar/index.jsx
@@ -11,36 +11,36 @@ export default class Navbar extends React.Component {
     constructor(props) {
         super(props);
     }
+    logout() {
+        console.log('退出了哦');
+    }
     render() {
-return(
-    <div className="navbar navbar-default top-navbar" role="navigation">
-            <div className="navbar-header">
-                <button type="button" className="navbar-toggle" data-toggle="collapse" data-target=".sidebar-collapse">
-                    <span className="sr-only">Toggle navigation</span>
-                    <span className="icon-bar"></span>
-                    <span className="icon-bar"></span>
-                    <span className="icon-bar"></span>
-                </button>
-                <a className="navbar-brand" to="/"><b>REACT</b>ADMIN</a>
+        return (
+            <div className="navbar navbar-default top-navbar" role="navigation">
+                <div className="navbar-header">
+                    <button type="button" className="navbar-toggle" data-toggle="collapse" data-target=".sidebar-collapse">
+                        <span className="sr-only">Toggle navigation</span>
+                        <span className="icon-bar"></span>
+                        <span className="icon-bar"></span>
+                        <span className="icon-bar"></span>
+                    </button>
+                    <a className="navbar-brand" to="/"><b>REACT</b>ADMIN</a>
+                </div>
+                <ul className="nav navbar-top-links navbar-right">
+                    <li className="dropdown">
+                        <a className="dropdown-toggle" data-toggle="dropdown" href="#" aria-expanded="false">
+                            <i className="fa fa-user fa-fw"></i>
+                            <span style={{ color: '#000000' }}>admin</span>
+                            <i className="fa fa-caret-down"></i>
+                        </a>
+                        <ul className="dropdown-menu dropdown-user">
+                            <li onClick={() => { this.logout() }}>
+                                <a href="#"><i className="fa fa-sign-out fa-fw"></i> 退出登录</a>
+                            </li>
+                        </ul>
+                    </li>
+                </ul>
             </div>
-
-            <ul className="nav navbar-top-links navbar-right">
-                <li className="dropdown">
-                    <a className="dropdown-toggle" data-toggle="dropdown" href="#" aria-expanded="false">
-                        <i className="fa fa-user fa-fw"></i> <i className="fa fa-caret-down"></i>
-                    </a>
-                    <ul className="dropdown-menu dropdown-user">
-                        <li><a href="#"><i className="fa fa-user fa-fw"></i> User Profile</a>
-                        </li>
-                        <li><a href="#"><i className="fa fa-gear fa-fw"></i> Settings</a>
-                        </li>
-                        <li className="divider"></li>
-                        <li><a href="#"><i className="fa fa-sign-out fa-fw"></i> Logout</a>
-                        </li>
-                    </ul>
-                </li>
-            </ul>
-        </div>
-)
+        )
     }
 }
