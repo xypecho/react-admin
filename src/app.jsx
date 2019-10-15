4064cac71f799eedebe38516bdd6557889fa4c77
xypecho <xypecho@163.com>
2019-10-15 21:17:34 +0800

    在项目中使用第三方react组件

--- src/app.jsx
+++ src/app.jsx
@@ -2,7 +2,7 @@
  * @Author: xueyp
  * @Date: 2019-09-23 21:39:05
  * @Last Modified by: xueyp
- * @Last Modified time: 2019-10-14 21:43:38
+ * @Last Modified time: 2019-10-15 20:26:56
  */
 import React from 'react';
 import ReactDom from 'react-dom';
@@ -10,6 +10,7 @@ import { BrowserRouter as Router, Route, Link, Redirect, Switch } from "react-ro
 import Home from 'page/home/index.jsx';
 import Layout from 'component/layout/index.jsx';
 import Login from 'page/login/index.jsx';
+import UserList from 'page/UserList/index.jsx';
 import ErrorPage from 'page/error/index.jsx';
 
 class App extends React.Component {
@@ -24,6 +25,9 @@ class App extends React.Component {
                                 <Route exact path="/" component={Home} />
                                 <Route path="/product" component={Home} />
                                 <Route path="/product-category" component={Home} />
+                                <Route path='/user/index' component={UserList}/>
+                                <Redirect exact from='/user' to='/user/index' />
+                                {/* 上面这段代码的意思是路由访问 '/user' 时会重定向到 '/user/index' */}
                                 <Route component={ErrorPage} />
                             </Switch>
                         </Layout>
---
+++ src/page/userList/index.jsx
@@ -0,0 +1,104 @@
+/*
+ * @Author: xueyp
+ * @Date: 2019-10-15 20:24:53
+ * @Last Modified by: xueyp
+ * @Last Modified time: 2019-10-15 21:16:30
+ * @description: 用户管理页面
+ */
+import React from 'react';
+import { Link } from 'react-router-dom';
+import PageTitle from 'component/layout/component/pageTitle/index.jsx'
+import Pagination from 'rc-pagination';
+import 'rc-pagination/dist/rc-pagination.min.css';
+
+export default class UserList extends React.Component {
+    constructor(props) {
+        super(props)
+    }
+    render() {
+        return (
+            <div className="row">
+                <div className="col-md-12">
+                    <PageTitle title='用户管理' />
+                    <div className="col-md-12">
+                        <div className="panel panel-default">
+                            <div className="panel-body">
+                                <div className="table-responsive">
+                                    <div id="dataTables-example_wrapper" className="dataTables_wrapper form-inline" role="grid"><table className="table table-striped table-bordered table-hover dataTable no-footer" id="dataTables-example" aria-describedby="dataTables-example_info">
+                                        <thead>
+                                            <tr role="row"><th className="sorting_asc" tabindex="0" aria-controls="dataTables-example" rowspan="1" colspan="1" aria-label="Rendering engine: activate to sort column ascending" aria-sort="ascending" >Rendering engine</th><th className="sorting" tabindex="0" aria-controls="dataTables-example" rowspan="1" colspan="1" aria-label="Browser: activate to sort column ascending" >Browser</th><th className="sorting" tabindex="0" aria-controls="dataTables-example" rowspan="1" colspan="1" aria-label="Platform(s): activate to sort column ascending" >Platform(s)</th><th className="sorting" tabindex="0" aria-controls="dataTables-example" rowspan="1" colspan="1" aria-label="Engine version: activate to sort column ascending">Engine version</th><th className="sorting" tabindex="0" aria-controls="dataTables-example" rowspan="1" colspan="1" aria-label="CSS grade: activate to sort column ascending" >CSS grade</th></tr>
+                                        </thead>
+                                        <tbody>
+                                            <tr className="gradeA odd">
+                                                <td className="sorting_1">Gecko</td>
+                                                <td className=" ">Firefox 1.0</td>
+                                                <td className=" ">Win 98+ / OSX.2+</td>
+                                                <td className="center ">1.7</td>
+                                                <td className="center ">A</td>
+                                            </tr><tr className="gradeA even">
+                                                <td className="sorting_1">Gecko</td>
+                                                <td className=" ">Firefox 1.5</td>
+                                                <td className=" ">Win 98+ / OSX.2+</td>
+                                                <td className="center ">1.8</td>
+                                                <td className="center ">A</td>
+                                            </tr><tr className="gradeA odd">
+                                                <td className="sorting_1">Gecko</td>
+                                                <td className=" ">Firefox 2.0</td>
+                                                <td className=" ">Win 98+ / OSX.2+</td>
+                                                <td className="center ">1.8</td>
+                                                <td className="center ">A</td>
+                                            </tr><tr className="gradeA even">
+                                                <td className="sorting_1">Gecko</td>
+                                                <td className=" ">Firefox 3.0</td>
+                                                <td className=" ">Win 2k+ / OSX.3+</td>
+                                                <td className="center ">1.9</td>
+                                                <td className="center ">A</td>
+                                            </tr><tr className="gradeA odd">
+                                                <td className="sorting_1">Gecko</td>
+                                                <td className=" ">Camino 1.0</td>
+                                                <td className=" ">OSX.2+</td>
+                                                <td className="center ">1.8</td>
+                                                <td className="center ">A</td>
+                                            </tr><tr className="gradeA even">
+                                                <td className="sorting_1">Gecko</td>
+                                                <td className=" ">Camino 1.5</td>
+                                                <td className=" ">OSX.3+</td>
+                                                <td className="center ">1.8</td>
+                                                <td className="center ">A</td>
+                                            </tr><tr className="gradeA odd">
+                                                <td className="sorting_1">Gecko</td>
+                                                <td className=" ">Netscape 7.2</td>
+                                                <td className=" ">Win 95+ / Mac OS 8.6-9.2</td>
+                                                <td className="center ">1.7</td>
+                                                <td className="center ">A</td>
+                                            </tr><tr className="gradeA even">
+                                                <td className="sorting_1">Gecko</td>
+                                                <td className=" ">Netscape Browser 8</td>
+                                                <td className=" ">Win 98SE+</td>
+                                                <td className="center ">1.7</td>
+                                                <td className="center ">A</td>
+                                            </tr><tr className="gradeA odd">
+                                                <td className="sorting_1">Gecko</td>
+                                                <td className=" ">Netscape Navigator 9</td>
+                                                <td className=" ">Win 98+ / OSX.2+</td>
+                                                <td className="center ">1.8</td>
+                                                <td className="center ">A</td>
+                                            </tr><tr className="gradeA even">
+                                                <td className="sorting_1">Gecko</td>
+                                                <td className=" ">Mozilla 1.0</td>
+                                                <td className=" ">Win 95+ / OSX.1+</td>
+                                                <td className="center ">1</td>
+                                                <td className="center ">A</td>
+                                            </tr></tbody>
+                                    </table>
+                                        <Pagination current={1} total={22} onChange={(current, pageSize)=>{console.log(current, pageSize)}}/>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        )
+    }
+}
